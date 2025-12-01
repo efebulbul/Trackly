dddaa5580cd4a6279697f6ab24d65bd038415391
Efe <efebulbull@icloud.com>
2025-12-01 16:13:22 +0300

    -

--- Features/Run/ogrenim.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-final class WelcomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-    }
-}
